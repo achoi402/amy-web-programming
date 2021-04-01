5f6b028f7e9238fdcfaa91003e146204de6a7d7b
achoi402 <79662804+achoi402@users.noreply.github.com>
2021-04-01 13:55:06 -0400

    yes

---
+++ refresh/script.js
@@ -0,0 +1,70 @@
+let string =
+  "either a mother and daughter know each other very well or they are strangers"
+let stringArray = string.split(" ");
+
+let caption = "don't you remember the words? find them again.";
+
+
+
+
+console.log(stringArray);
+var newdiv;
+for (let i = 0; i < stringArray.length; i++) {
+  let newdiv = document.createElement("div");
+
+  newdiv.innerHTML = stringArray[i];
+
+  newdiv.classList.add("wordItem");
+  $("h1").click(function() {
+newdiv.style.top = getRandomNumber(200, window.innerHeight-200) + "px";
+    newdiv.style.left = getRandomNumber(0, window.innerWidth-200) + "px";
+});
+  
+  newdiv.style.top = getRandomNumber(200, window.innerHeight-200) + "px";
+    newdiv.style.left = getRandomNumber(0, window.innerWidth-200) + "px";
+  // newdiv.style.top = getRandomNumber(200, window.innerHeight) + "px";
+  // newdiv.style.right = getRandomNumber(100, window.innerWidth) + "px";
+  // newdiv.style.right = getRandomNumber(-100,window.innerWidth) + "px";
+  
+  document.body.appendChild(newdiv);
+  
+   newdiv.onclick = () => {
+      newdiv.classList.toggle("clicked");
+    };
+}
+  
+// animation(newdiv);
+//   }
+
+
+let todo = document.getElementById("todo");
+  console.log (todo);
+
+ $("h1").click(function() {
+   todo.innerHTML = caption;
+
+ })
+
+
+// function animation(element){
+//     for (let i = 0; i<1000; i++){
+//     $(element).animate({left: '+='+getRandomJiggle(), top: '-='+getRandomJiggle()}, 200);
+//     $(element).animate({left: '-='+getRandomJiggle(), top: '+='+getRandomJiggle()}, 200);
+//   }
+
+// }
+
+function getRandomNumber(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+
+
+// function getRandomJiggle() {
+//   return Math.random() * 0.5 + "px";
+// }
+
+
+
+
+
